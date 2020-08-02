7e8b71f8934ffbf5c76ed1c3c32b9dbb0aa5a42f
Bruno Terry Alvarado <brunoterry@upeu.edu.pe>
2020-08-02 16:08:59 -0500

    version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Menu2Component } from './menu2/menu2.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'Inicio' ,component: AppComponent},
+                        {path:'Catag' ,component: Menu2Component}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { CompraComponent } from './compra/compra.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
+import { Menu2Component } from './menu2/menu2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    CompraComponent,
+    ContactoComponent,
+    IndexComponent,
+    LoginComponent,
+    PiePaginaComponent,
+    Menu2Component
   ],
   imports: [
     BrowserModule,
